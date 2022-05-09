62237d550ff544a7cc591de622b6de599809065c
frangdelsolar <frangdelsolar@gmail.com>
2022-05-09 01:40:29 -0300

    gamestate and test

--- game_state.py
+++ game_state.py
@@ -5,7 +5,6 @@ from pawn import Pawn
 class GameState:
     def __init__(self):
         self.state = [[0] * config.COLS for i in range(config.ROWS)]
-        self.turn = None
         self.player = '' # S or N
         self.opponent = '' # S or N
         self.player_pawns = []
@@ -14,7 +13,7 @@ class GameState:
     def update(self, data):
         self.state = self.decode_board(data['board'])
         self.player = data['side']
-        self.opponent = 'N' if self.player == 'S' else 'S'
+        self.opponent = 'N' if data['side'] == 'S' else 'S'
         self.player_pawns = self.create_pawns(self.player)
         self.opponent_pawns = self.create_pawns(self.opponent)
 
@@ -23,9 +22,10 @@ class GameState:
         for i, row in enumerate(self.state):
             for j, item in enumerate(row):
                 if item == side:
-                    pawns.append(Pawn(i, j, side))
+                    pawn = Pawn(i, j, side)
+                    pawns.append(pawn)
         return pawns
-
+    
     def decode_board(self, encoded):
         state = [[0] * config.COLS for i in range(config.ROWS)]
         for i in range(len(encoded)):
@@ -44,44 +44,35 @@ class GameState:
                 encoded += state[i][j]
         return encoded
 
-    def analize(self):
-        
-
     def move(self):
-        pass
+        move_1 = self.player_pawns[0].move(self.state)
+        move_2 = self.player_pawns[1].move(self.state)
+        move_3 = self.player_pawns[2].move(self.state)
 
-    # def move(self):
-    #     pawn_1 = self.pawns[0]
-    #     pawn_2 = self.pawns[1]
-    #     pawn_3 = self.pawns[2]
+        move = None
+        if move_1:
+            move = move_1
+
+        if move:
+            if move_2:
+                if move_2['distance'] < move['distance']:
+                    move = move_2
+        else:
+            if move_2:
+                move = move_2
         
-    #     move_1 = pawn_1.move(self.state)
-    #     move_2 = pawn_2.move(self.state)
-    #     move_3 = pawn_3.move(self.state)
-
-
-    #     # move_1 = pawn_1.bfs(self.state)
-    #     # move_2 = pawn_2.bfs(self.state)
-    #     # move_3 = pawn_3.bfs(self.state)
-    #     # all_paths = move_1 + move_2 + move_3
-    #     # self.show(all_paths)
-
-    #     move = None
-    #     if move_1:
-    #         move = move_1
-
-    #     if move_2 and move_2['distance'] < move['distance']:
-    #         move = move_2
         
-    #     if move_3 and move_3['distance'] < move['distance']:
-    #         move = move_3
+        if move:
+            if move_3:
+                if move_3['distance'] < move['distance']:
+                    move = move_3
+        else:
+            if move_3:
+                move = move_3
 
-    #     # pawn = random.choice(self.pawns)
-    #     # move = pawn.move(self.state)
-
-    #     if move:
-    #         move.pop('distance', None)
-    #     return move
+        if move:
+            move.pop('distance', None)
+        return move
 
 
     def show(self, paths=None):
